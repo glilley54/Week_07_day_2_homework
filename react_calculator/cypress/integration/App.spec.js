20739df622a5fdcadcbf7a6c28485878f82391d8
glilley54 <glilley54@gmail.com>
2021-05-12 21:45:29 +0100

    first test passed in cypress

--- react_calculator/cypress/integration/App.spec.js
+++ react_calculator/cypress/integration/App.spec.js
@@ -7,4 +7,13 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display of the running total', () => {
+    cy.get('#number7').click();
+    cy.get('#number5').click();
+    cy.get('#number9').click();
+    cy.get('.display').should('contain', '759')
+
+  
+  })
 })
