a8dc2cf19f278301dfda9444310530414fff139b
glilley54 <glilley54@gmail.com>
2021-05-12 19:57:21 +0100

    3 tests passing

--- react_calculator/src/tests/App.test.js
+++ react_calculator/src/tests/App.test.js
@@ -13,12 +13,38 @@ describe('Calculator', () => {
     fireEvent.click(button4);
     expect(runningTotal).toHaveTextContent('4');
     
-  })
-
-  it('should be able to add 1 to 4 and get 5', () => {
-    const add = container.getByTestId("#add");
-    expect(add.text()).toEqual('5');
   });
 
-})
+  it('should add 1 and 4 to get 5', () => {
+    const button1 = container.getByTestId('number1');
+    const buttonAdd = container.getByTestId('add');
+    const button4 = container.getByTestId('number4');
+    const buttonEquals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(buttonAdd);
+    fireEvent.click(button4);
+    fireEvent.click(buttonEquals);
+    expect(runningTotal).toHaveTextContent('5'); 
+  
+  });
+  
+  it ('subtract 4 from 7 and get 3', () => {
+      const button7 = container.getByTestId('number7');
+      const buttonSubtract = container.getByTestId('subtract');
+      const button4 = container.getByTestId('number4');
+      const buttonEquals = container.getByTestId('equals');
+      const runningTotal = container.getByTestId('running-total');
+      fireEvent.click(button7);
+      fireEvent.click(buttonSubtract);
+      fireEvent.click(button4);
+      fireEvent.click(buttonEquals);
+      expect(runningTotal).toHaveTextContent('3');
+  
+    
+  });
+
+}) 
+    
+
 
